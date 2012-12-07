4c221978e9f23dcc9507048b989b5f9e7f6cd34e
Steven Goeldner <sg@campoint.net>
2012-12-07 10:18:36 +0100

    blog c copy

---
+++ http-root/blog/c4.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload_register(function ($class) {
+	$map = array(
+		'Author' => '/b1.php',
+		'Blog' => '/b2.php',
+		'Post' => '/b3.php'
+	);
+
+	if(isset($map[$class])) {
+		include(__DIR__ . $map[$class]);
+	}
+});
